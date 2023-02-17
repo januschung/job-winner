58b69d7fa16b803c3fe0698b4d555d02995e48c7
Janus Chung <januschung@gmail.com>
2023-02-16 23:56:02 -0800

    add graphql unit test

--- src/main/java/com/tnite/jobwinner/repo/JobApplicationRepository.java
+++ src/main/java/com/tnite/jobwinner/repo/JobApplicationRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import com.tnite.jobwinner.model.JobApplication;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface JobApplicationRepository extends ReactiveCrudRepository<JobApplication, Integer>{
-    Flux<JobApplication> getJobApplicationByCompanyName(String companyName);
 
 }
